900202d026123d7d266eb6c7a57ee6971a2c3333
Dean Birnie <110895522+deanbirnie@users.noreply.github.com>
2023-10-27 23:38:21 +0200

    Remove URL

--- backend/config.js
+++ backend/config.js
@@ -3,4 +3,4 @@
 
 export const PORT = 5555;
 
-export const mongodbURL = 'mongodb+srv://dean:DeanB5140088!@cluster0.pirvkyy.mongodb.net/daily-a?retryWrites=true&w=majority';
+export const mongodbURL = ATLAS_URL;
