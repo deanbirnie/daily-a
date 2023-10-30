6df7e6978c5fe32d5414fb9c7f2700a2bfaf942c
Dean Birnie <110895522+deanbirnie@users.noreply.github.com>
2023-10-30 20:03:47 +0200

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-export const PORT = 5555;
-
-export const mongodbURL = 'mongodb+srv://adminUser:DHw7HpeSjGpO3IsI@cluster0.pirvkyy.mongodb.net/daily-a?retryWrites=true&w=majority';
-
-export const JWT_SECRET = "PHyHuA2VuKjvTYoA2GtrBaqvZCFUglCJ";
