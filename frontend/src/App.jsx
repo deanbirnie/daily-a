fd27bc1cb029563a54501cadc409fadef6a23fe9
deanbirnie <deanicdl@gmail.com>
2023-11-05 17:06:28 +0200

    Create pages and basic page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './pages/home';
+import About from './pages/about';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 export default function App() {
-  return (
-    <h1 className='text-red-400'>App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/sign-in' element={<Signin />} />
+    <Route path='/sign-up' element={<Signup />} />
+  </Routes>
+  </BrowserRouter>
 }
