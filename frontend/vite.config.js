401d6b18579ec8831d7c25476f60bcdd1a4c5483
deanbirnie <deanicdl@gmail.com>
2023-11-06 11:11:18 +0200

    Adjusted Vite Config for proxy to backend server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { PORT } from '../backend/app.config.js'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
