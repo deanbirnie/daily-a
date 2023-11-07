a10a92d51949a244aa03bdb7898c7303ac801156
deanbirnie <deanicdl@gmail.com>
2023-11-07 19:05:35 +0200

    Implement User sign out from profile page and include basic user info on profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOutUserStart,
+    signOutUserSuccess,
+    signOutUserFailure } from '../redux/user/user.slice.js';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+    const { currentUser, loading, error } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSignOut = async () => {
+        try {
+            dispatch(signOutUserStart());
+            const response = await fetch('/api/auth/sign-out');
+            const data = await response.json();
+            if (data.success === false) {
+                dispatch(signOutUserFailure(data.message));
+                return;
+            }
+            dispatch(signOutUserSuccess());
+        } catch (error) {
+            dispatch(signOutUserFailure(error.message));
+        };
+    };
+    return (
+        <div className='p-3 max-w-lg mx-auto flex flex-col'>
+            <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
+            <img src={ currentUser.profilePhoto }
+                alt='profile'
+                className='rounded-full h-24 w-24 object-cover cursor-pointer self-center my-2'
+            />
+            <h3 className='text-3xl text-center font-semibold my-7'>User: { currentUser.username }</h3>
+            <h3 className='text-3xl text-center font-semibold my-7'>Email: { currentUser.email }</h3>
+            <form onSubmit={handleSignOut} className='flex flex-col gap-4'>
+                <button className='bg-red-700 uppercase text-white p-3 mt-7 rounded-lg text-center hover:opacity-90'>Sign Out</button>
+            </form>
+            <div className='flex mt-5'>
+                <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
+            </div>
+        </div>
+    )
+};
