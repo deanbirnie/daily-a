c4e9211fe2c09369fc1050484f128586e2d89fc7
deanbirnie <deanicdl@gmail.com>
2023-11-15 22:55:44 +0200

    Added example config file with no secrets

---
+++ backend/example.app.config.js
@@ -0,0 +1,7 @@
+// export const PORT = 1234;
+
+export const mongodbURL = 'MONGODB_CONNECTION_STRING';
+
+export const JWT_SECRET = "RANDOM_STRING";
+
+export const FIREBASE_API_KEY = "FIREBASE_API_KEY";
